a642cca216bf1ae503818113d992676795b8b30e
rahul sah <rahul.sah089@gmail.com>
2016-12-19 14:34:45 +0530

    Hibernate Collection Mapping

---
+++ HibernateAssociation/src/main/java/pojo/Player.java
@@ -0,0 +1,41 @@
+package pojo;
+
+public class Player {
+	private int pid;
+	private String pname;
+	private int age;
+	private int tid;
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getTid() {
+		return tid;
+	}
+
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+
+}
